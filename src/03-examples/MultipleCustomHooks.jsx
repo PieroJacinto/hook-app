5a492a32db7ff341004eb82416c012a728c843f2
PieroJacinto <pierojacinto@gmail.com>
2023-07-19 13:09:01 -0300

    components add

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,37 +1,39 @@
-import { useFetch } from "../hooks/useFetch"
+import { useState } from "react";
+import { useFetch, useCounter } from "../hooks";
+import { LoadingQuote, Quote } from './';
+
 
 export const MultipleCustomHooks = () => {
+  const { data, isLoading, hasError } = useFetch(
+    "https://api.breakingbadquotes.xyz/v1/quotes/20"
+  );
+  const [quoteCounter, setQuoteCounter] = useState(0);
+  const { author, quote } = !!data && data[quoteCounter];
 
-    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/20');
+  const handleNextQuote = () => {
+    if (quoteCounter < data.length - 1) {
+      setQuoteCounter(quoteCounter + 1);
+    } else {
+      // Si llega al final de las citas, puedes optar por mostrar un mensaje o reiniciar el contador.
+      // Aquí, simplemente reiniciaremos el contador a 0.
+      setQuoteCounter(0);
+    }
+  };
 
-    const { author, quote } = !!data && data[0]
+  return (
+    <>
+      <h1> BreakingBad Quotes </h1>
+      <hr />
 
-    
+      {
+      isLoading 
+        ? < LoadingQuote />
+        : < Quote author = { author } quote = { quote }/>
+      }
 
-    return (
-        <>
-            <h1> BreakingBad Quotes </h1>
-            <hr />
-
-            {
-                isLoading 
-                    ? (
-                        <div className="alert alert-info text-center">
-                            Loading...
-                        </div>
-
-                    )
-                    : (
-                        <blockquote className="blockquote text-end">
-                            <p className="mb-1"> { quote } </p>
-                            <footer className="blockquote-footer"> { author }</footer>
-                        </blockquote>
-                    )
-            }
-
-            <button className="btn btn-primary"> 
-                Next Quote 
-            </button>
-        </>
-    )
-}
+      <button className="btn btn-primary" onClick={handleNextQuote}>
+        Next Quote
+      </button>
+    </>
+  );
+};
